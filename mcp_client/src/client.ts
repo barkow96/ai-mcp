748f7255ad9b86059c044f11f80ee177418356e9
Bartosz Kowal <bartoszkowal96@gmail.com>
2025-07-30 22:03:44 +0200

    feat(mcp-client-tools): Implement client-side usage of server tools

--- mcp_client/src/client.ts
+++ mcp_client/src/client.ts
@@ -1,6 +1,7 @@
-import { select } from '@inquirer/prompts';
+import { input, select } from '@inquirer/prompts';
 import { Client } from '@modelcontextprotocol/sdk/client/index.js';
 import { StdioClientTransport } from '@modelcontextprotocol/sdk/client/stdio.js';
+import { Tool } from '@modelcontextprotocol/sdk/types.js';
 
 const client = new Client({
   name: 'mcp-client',
@@ -35,7 +36,45 @@ async function main() {
       message: 'What would you like to do?',
       choices: ['Query', 'Tools', 'Resources', 'Prompts'],
     });
+
+    switch (option) {
+      case 'Tools':
+        const toolName = await select({
+          message: 'Select a tool to use:',
+          choices: tools.map((tool) => ({
+            name: tool.annotations?.title || tool.name,
+            value: tool.name,
+            description: tool.description,
+          })),
+        });
+        console.log(toolName);
+
+        const tool = tools.find((t) => t.name === toolName);
+
+        if (!tool) {
+          console.error('Tool not found.');
+        } else {
+          await handleTool(tool);
+        }
+        break;
+    }
   }
+}
+
+async function handleTool(tool: Tool) {
+  const args: Record<string, string> = {};
+
+  for (const [key, value] of Object.entries(
+    tool.inputSchema.properties ?? {},
+  )) {
+    args[key] = await input({
+      message: `Enter value for ${key} (${(value as { type: string }).type}):`,
+    });
+  }
+
+  const res = await client.callTool({ name: tool.name, arguments: args });
+
+  console.log((res.content as [{ text: string }])[0].text);
 }
 
 main();
